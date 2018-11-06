ccd3b3cfc26cf7e99f20bd64934c1d7caf94710d
ilyshander <mr.iluxin@yandex.ru>
2018-11-06 22:01:04 +0300

    zabil testi v papky

--- Сысоев/base_test/test_tpostfix.cpp
+++ Сысоев/base_test/test_tpostfix.cpp
@@ -1,8 +1,76 @@
-#include "postfix.h"
+#include "postfix.cpp"
 #include <gtest.h>
-
-TEST(TPostfix, can_create_postfix)
+TEST(TPostfix, can_create_postfix) 
 {
-  ASSERT_NO_THROW(TPostfix p);
+	ASSERT_NO_THROW(TPostfix p);
 }
-
+TEST(TPostfix, can_add_infix) 
+{
+	TPostfix p;
+	p.Set("3 + 4");
+	EXPECT_EQ("3 + 4", p.GetInfix());
+}
+TEST(TPostfix, can_get_postfix) 
+{
+	TPostfix p("1 + 5");
+	EXPECT_EQ("1_5_+", p.GetPostfix());
+}
+TEST(TPostfix, can_get_postfix_from_expression_with_two_characters_of_the_same_order) 
+{
+	TPostfix p("1 + 2 * 3 / 4 - 5");
+	EXPECT_EQ("1_2_3_*_4_/_+_5_-", p.GetPostfix());
+}
+TEST(TPostfix, can_create_postfix_from_postfix) 
+{
+	TPostfix p1("1 + 5");
+	p1.GetPostfix();
+	TPostfix p2(p1);
+	EXPECT_EQ("1 + 5", p2.GetInfix());
+	EXPECT_EQ("1_5_+", p2.GetPostfix());
+}
+TEST(TPostfix, can_assign_postfix) 
+{
+	TPostfix p1("1 + 5");
+	TPostfix p2;
+	p1.GetPostfix();
+	p2 = p1;
+	EXPECT_EQ("1 + 5", p2.GetInfix());
+	EXPECT_EQ("1_5_+", p2.GetPostfix());
+}
+TEST(TPostfix, can_calculate_simple_expression) 
+{
+	TPostfix p("11 - 5");
+	EXPECT_EQ(6, p.Calculate());
+}
+TEST(TPostfix, can_calculate_expression_with_brackets) 
+{
+	TPostfix p("20 / (6 + 4)");
+	EXPECT_EQ(2, p.Calculate());
+}
+TEST(TPostfix, can_calculate_expression_with_two_characters_of_the_same_order) 
+{
+	TPostfix p("1 + 2 * 3 / 4 - 5");
+	EXPECT_EQ(-2.5, p.Calculate());
+}
+TEST(TPostfix, can_not_divide_by_zero) 
+{
+	TPostfix p("20 / 0");
+	ASSERT_ANY_THROW(p.Calculate());
+}
+TEST(TPostfix, can_reset_expression) 
+{
+	TPostfix p("4 - 5");
+	p.Calculate();
+	p.Set("1 - 3");
+	EXPECT_EQ(-2, p.Calculate());
+}
+TEST(TPostfix, throw_if_expression_has_non_number_member) 
+{
+	TPostfix p("5 + 4 - 3 + a");
+	ASSERT_ANY_THROW(p.Calculate());
+}
+TEST(TPostfix, can_calculate_double) 
+{
+	TPostfix p("1.34 + 5.24");
+	EXPECT_EQ(6.58, p.Calculate());
+}
--- Сысоев/base_test/test_tstack.cpp
+++ Сысоев/base_test/test_tstack.cpp
@@ -1,7 +1,63 @@
 #include "stack.h"
 #include <gtest.h>
-
 TEST(TStack, can_create_stack_with_positive_length)
 {
-  ASSERT_NO_THROW(TStack<int> st(5));
+	ASSERT_NO_THROW(TStack<int> st(5));
 }
+TEST(TStack, can_not_create_stack_with_negative_length) 
+{
+	ASSERT_ANY_THROW(TStack<int> st(-1));
+}
+TEST(TStack, can_not_create_stack_with_too_large_length) 
+{
+	ASSERT_ANY_THROW(TStack<int> st(1000));
+}
+TEST(TStack, can_assign_stack_with_equal_size) 
+{
+	TStack<int> st(5);
+	TStack<int> st1(5);
+	st.push_back(5);
+	st1 = st;
+	EXPECT_EQ(5, st1.pop_back());
+}
+TEST(TStack, can_assign_stack_with_not_equal_size) 
+{
+	TStack<int> st(4);
+	TStack<int> st1(2);
+	st.push_back(4);
+	st1 = st;
+	EXPECT_EQ(4, st1.pop_back());
+}
+TEST(TStack, assign_change_stack_size) 
+{
+	TStack<int> st(4);
+	TStack<int> st1(2);
+	st1 = st;
+	EXPECT_EQ(4, st1.Size());
+}
+TEST(TStack, can_add_item) 
+{
+	TStack<int> a(5);
+	a.push_back(4);
+	EXPECT_EQ(4, a.pop_back());
+}
+TEST(TStack, can_not_add_item_if_stack_is_full) 
+{
+	TStack<int> st1(1);
+	st1.push_back(1);
+	ASSERT_ANY_THROW(st1.push_back(1));
+}
+TEST(TStack, can_get_item) 
+{
+	TStack<int> a(1);
+	a.push_back(4);
+	EXPECT_EQ(4, a.watch_top());
+	EXPECT_EQ(4, a.pop_back());
+}
+TEST(TStack, can_not_get_item_if_stack_is_empty) 
+{
+	TStack<int> a(1);
+	a.push_back(4);
+	a.pop_back();
+	ASSERT_ANY_THROW(a.pop_back());
+} 
